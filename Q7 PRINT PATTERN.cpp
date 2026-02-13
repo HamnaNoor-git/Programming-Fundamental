cab9fbe868d3f410c0fd9ede94aa98f3c893024d
Hamna Noor <hamna10106noor@gmail.com>
2026-02-13 16:49:56 +0500

    Add files via upload

---
+++ Q7 PRINT PATTERN.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char s;
+	cin >> s;
+	cout << "    "<<s<<"      "<<s<<" "<<s<<" "<<s<<" "<<s<<" "<<s<<endl;
+	cout << "   "<<s<<" "<<s<<"      "<<s<<" "<<s<<" "<<s<<" "<<s<<endl;
+	cout << "  "<<s<<" "<<s<<" "<<s<<"      "<<s<<" "<<s<<" "<<s<<endl;
+	cout <<" "<<s<<" "<<s<<" "<<s<<" "<<s<<"      "<<s<<" "<<s<<endl;
+	cout << s<<" "<<s<<" "<<s<<" "<<s<<" "<<s<<"      "<<s;
+	
+	return 0;
+}
